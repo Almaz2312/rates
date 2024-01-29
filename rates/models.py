51fe68f09041e4f9dcb6660926ad1820a71b9256
Almaz Durusaliev <{ID}+{username}@users.noreply.github.com>
2024-01-29 13:32:16 +0600

    fix: remove commented data

--- rates/models.py
+++ rates/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
 
-# {
-#   "RUB to USD rate": 0.0111,
-#   "date": "2024-01-29"
-# }
-
 class FxRate(models.Model):
     rate = models.CharField(max_length=24)
     date_of_query = models.DateField()
